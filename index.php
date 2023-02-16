b310666d69b4ded4f0daa0a4e549f5d6af7f3899
Juscelino Campos <celino3x@gmail.com>
2023-02-16 15:30:03 -0300

    Desenvolvimento de página de login/registro - Estilo e layout

--- index.php
+++ index.php
@@ -8,7 +8,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.1/dist/sweetalert2.all.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.1/dist/sweetalert2.min.css">
+    <script src="js/jquery-3.6.3.min.js"></script>
+    <script src="js/sweetalert2.all.min.js"></script>
     <title>Chat PHP</title>
 </head>
 <body>
