644e60d3f80d0869805acd343cb4c77b3b860595
Juscelino Campos <celino3x@gmail.com>
2023-02-16 18:47:04 -0300

    registration

---
+++ process/connection/registration.php
@@ -0,0 +1,14 @@
+<?php
+    include("connection/connect.php");
+
+    if(isset($_POST["username"]) && isset($_POST($_POST["email"]) && isset($_POST["password"]) && isset($_POST["repPassword"])){
+        $username = $_POST["username"];
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+        $repPassword = $_POST["repPassword"];
+
+        if($username == "" || $email == "" || $password = "" || $repPassword == ""){
+
+        }
+    }
+?>
