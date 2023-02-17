65d69ecf352803236e94059d95d0b5c03fcc7465
Juscelino Campos <celino3x@gmail.com>
2023-02-17 18:42:35 -0300

    modificações em login e registration

--- process/connection/connect.php
+++ process/connection/connect.php
@@ -1,5 +1,6 @@
 <?php
-    $con = mysqli_connect("mysql367.umbler.com", "celino3x", "JC123456", "quicktalk");
+    //$con = mysqli_connect("mysql367.umbler.com", "celino3x", "JC123456", "quicktalk");
+    $con = mysqli_connect("localhost", "celino3x", "123456", "quicktalk");
     mysqli_query($con, "SET time_zone-'+00:00'");
 
     date_default_timezone_set("UTC");
--- process/connection/login.php
+++ process/connection/login.php
@@ -1,28 +1,32 @@
 <?php
     include("connection/connect.php");
 
-    if(isset($_POST["email"]) && isset($_POST["password"])){
+    if(isset($_POST["email"]) && isset($_POST["password"])) {
+        // Normalization
         $email = $_POST["email"];
         $password = $_POST["password"];
 
-        if($email == "" || $password == ""){
-            die(header("HTTP/1.0 401 Preencha todos os campos do formulário"));
+        // Check if values are okay
+        if ($email == "" || $password == "") {
+            die(header("HTTP/1.0 401 Preenche todos os campos do formulário"));
         }
 
-        $stmt = $con->prepare("SELECT id, senha, token, seguro FROM user WHERE (email LIKE ? OR usuario LIKE ?) LIMIT 1"); 
+        // Query
+        $stmt = $con->prepare("SELECT Id, Password, Token, Secure FROM User WHERE (Email LIKE ? OR Username LIKE ?) LIMIT 1");
         $stmt->bind_param("ss", $email, $email);
         $stmt->execute();
         $user = $stmt->get_result()->fetch_assoc();
 
-        if($user && passord_verify($password, $user["senha"])){
-            setcookie("ID", $user["id"], time() + (10* 365 * 24 * 60 * 60));
-            setcookie("TOKEN", $user["token"], time() + (10* 365 * 24 * 60 * 60));
-            setcookie("SEGURO", $user["seguro"], time() + (10* 365 * 24 * 60 * 60));
+        // Check password
+        if ($user && password_verify($password, $user['Password'])) {
+            setcookie("ID", $user['Id'], time() + (10 * 365 * 24 * 60 * 60));
+            setcookie("TOKEN", $user['Token'], time() + (10 * 365 * 24 * 60 * 60));
+            setcookie("SECURE", $user['Secure'], time() + (10 * 365 * 24 * 60 * 60));
             return true;
-        } else{
-            die(header("HTTP/1.0 401 Senha errada"));
+        } else {
+            die(header("HTTP/1.0 401 Password incorreta"));
         }
     } else {
-        die(header("HTTP/1.0 401 Formulário de Autenticação Inválido"));
+        die(header("HTTP/1.0 401 Formulário de autenticação inválido"));
     }
 ?>
